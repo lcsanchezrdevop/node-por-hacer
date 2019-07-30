7e1e245853a8530bdaf101b729cc9ec3a1919abb
Luis Carlos Sanchez <lcsanchezr@hotmail.com>
2019-07-29 20:41:53 -0500

    Primer commit

---
+++ README.md
@@ -0,0 +1,9 @@
+## Aplicacion de comandos
+
+Este es el ejercicio del curso
+
+Recuerde instalar los paquetes de Node asi
+
+```
+npm install
+```
---
+++ config/yargs.js
@@ -0,0 +1,29 @@
+const descripcion = {
+    demand: true,
+    alias: 'd',
+    desc: 'Descripción de la tarea por hacer'
+};
+
+const completado = {
+    alias: 'c',
+    default: true,
+    desc: 'Marca como completado o pendiente la tarea'
+};
+
+const argv = require('yargs')
+    .command('crear', 'Crear tarea por hacer', {
+        descripcion
+    })
+    .command('actualizar', 'Actualiza una tarea por hacer', {
+        descripcion,
+        completado
+    })
+    .command('borrar', 'Elimina el registro de la BD', {
+        descripcion
+    })
+    .help()
+    .argv;
+
+module.exports = {
+    argv
+}
